b2bd09811984fe01585fabcd9baefdac0af7d589
yoezequiel <email>
2023-07-16 16:48:45 -0300

    update

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
-from config import SECRET_KEY
 from flaskext.markdown import Markdown
-import sqlite3
-
+import mysql.connector
+from mysql.connector import Error
+from config import SECRET_KEY, DB_CONFIG
 
 app = Flask(__name__)
 app = Flask(__name__, static_url_path='/static')
@@ -10,106 +10,157 @@ app.secret_key = SECRET_KEY  # Clave secreta para la sesión
 
 Markdown(app)
 
+# Función para obtener la conexión a la base de datos
+def get_db_connection():
+    try:
+        conn = mysql.connector.connect(**DB_CONFIG)
+        return conn
+    except Error as e:
+        print(e)
+        return None
+
 # Rutas
 @app.route('/')
 def index():
-    # Crea una nueva conexión y cursor en la función de la ruta
-    conn = sqlite3.connect('blog.db')
-    cursor = conn.cursor()
+    conn = get_db_connection()
+    if conn:
+        try:
+            cursor = conn.cursor()
 
-    # Crear la tabla "articles" si no existe
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS articles (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT NOT NULL,
-            content TEXT NOT NULL,
-            image TEXT
-        )
-    ''')
-    conn.commit()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS articles (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    title TEXT NOT NULL,
+                    content TEXT NOT NULL,
+                    image TEXT
+                )
+            ''')
+            conn.commit()
 
-    cursor.execute("SELECT * FROM articles ORDER BY id DESC LIMIT 5")
-    latest_articles = cursor.fetchall()
+            cursor.execute("SELECT * FROM articles ORDER BY id DESC LIMIT 5")
+            latest_articles = cursor.fetchall()
 
-    # Cierra la conexión y el cursor después de usarlos
-    cursor.close()
-    conn.close()
+            cursor.close()
+            conn.close()
 
-    if len(latest_articles) == 0:
-        message = "No hay artículos publicados aún"
+            if len(latest_articles) == 0:
+                message = "No hay artículos publicados aún"
+            else:
+                message = ""
+
+            return render_template('index.html', articles=latest_articles, message=message)
+
+        except Error as e:
+            print(e)
+            return "Error en la conexión a la base de datos"
     else:
-        message = ""
-
-    return render_template('index.html', articles=latest_articles, message=message)
+        return "Error en la conexión a la base de datos"
 
 
 @app.route('/articles')
 def view_all_articles():
-    conn = sqlite3.connect('blog.db')
-    cursor = conn.cursor()
+    conn = get_db_connection()
+    if conn:
+        try:
+            cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM articles")
-    all_articles = cursor.fetchall()
+            cursor.execute("SELECT * FROM articles")
+            all_articles = cursor.fetchall()
 
-    cursor.close()
-    conn.close()
+            cursor.close()
+            conn.close()
 
-    return render_template('all_articles.html', articles=all_articles)
+            return render_template('all_articles.html', articles=all_articles)
+
+        except Error as e:
+            print(e)
+            return "Error en la conexión a la base de datos"
+    else:
+        return "Error en la conexión a la base de datos"
 
 
 @app.route('/article/<int:article_id>')
 def view_article(article_id):
-    conn = sqlite3.connect('blog.db')
-    cursor = conn.cursor()
+    conn = get_db_connection()
+    if conn:
+        try:
+            cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM articles WHERE id = ?", (article_id,))
-    article = cursor.fetchone()
+            cursor.execute("SELECT * FROM articles WHERE id = %s", (article_id,))
+            article = cursor.fetchone()
 
-    cursor.close()
-    conn.close()
+            cursor.close()
+            conn.close()
 
-    return render_template('article.html', article=article)
+            return render_template('article.html', article=article)
+
+        except Error as e:
+            print(e)
+            return "Error en la conexión a la base de datos"
+    else:
+        return "Error en la conexión a la base de datos"
 
 
 @app.route('/search')
 def search():
     keyword = request.args.get('keyword', '')
 
-    conn = sqlite3.connect('blog.db')
-    cursor = conn.cursor()
+    conn = get_db_connection()
+    if conn:
+        try:
+            cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM articles WHERE title LIKE ?", ('%' + keyword + '%',))
-    articles = cursor.fetchall()
+            cursor.execute("SELECT * FROM articles WHERE title LIKE %s", ('%' + keyword + '%',))
+            articles = cursor.fetchall()
 
-    cursor.close()
-    conn.close()
+            cursor.close()
+            conn.close()
 
-    return render_template('search.html', articles=articles)
+            return render_template('search.html', articles=articles)
+
+        except Error as e:
+            print(e)
+            return "Error en la conexión a la base de datos"
+    else:
+        return "Error en la conexión a la base de datos"
 
 
 @app.route('/create_article', methods=['GET', 'POST'])
 def create_article():
     if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-        image = request.files.get('image')  # Usamos get() en lugar de ['image'] para obtener el archivo opcionalmente
+        try:
+            title = request.form['title']
+            content = request.form['content']
+            image = request.files.get('image')  # Usamos get() en lugar de ['image'] para obtener el archivo opcionalmente
 
-        if image:
-            # Guardar la imagen en tu sistema de archivos
-            image.save('static/images/' + image.filename)
+            if image:
+                # Guardar la imagen en tu sistema de archivos
+                image.save('static/images/' + image.filename)
 
-        conn = sqlite3.connect('blog.db')
-        cursor = conn.cursor()
+            conn = get_db_connection()
+            if conn:
+                try:
+                    cursor = conn.cursor()
 
-        # Insertar el artículo en la base de datos
-        cursor.execute("INSERT INTO articles (title, content, image) VALUES (?, ?, ?)",
-                    (title, content, image.filename if image else None))
-        conn.commit()
+                    # Insertar el artículo en la base de datos
+                    cursor.execute("INSERT INTO articles (title, content, image) VALUES (%s, %s, %s)",
+                                (title, content, image.filename if image else None))
+                    conn.commit()
 
-        cursor.close()
-        conn.close()
+                    cursor.close()
+                    conn.close()
 
-        return "Artículo creado exitosamente"
+                    return "Artículo creado exitosamente"
+
+                except Error as e:
+                    print(e)
+                    return "Error en la conexión a la base de datos"
+            else:
+                return "Error en la conexión a la base de datos"
+
+        except Error as e:
+            print(e)
+            return "Error en la conexión a la base de datos"
 
     return render_template('create_article.html')
 
@@ -121,8 +172,8 @@ def cursos():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 40
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 SECRET_KEY = 'csBigWQbhkGYUvqENmmmdeyYsGBCxHRMPWRNuVCpFCruyNXhfFmLSmvPYUXRftdfrZckUZcLVmYriizdUiMgagCRtEDGqEGiMBMwSmZhTJqXKHnVcLgCqfPscdegRPXc'
+DB_CONFIG = {
+    "host": "yoezequiel.mysql.pythonanywhere-services.com",
+    "database": "yoezequiel$blog",
+    "user": "yoezequiel",
+    "password": "^b8yY%Z6ZP8ap8x*"
+}
