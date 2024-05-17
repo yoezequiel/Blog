64be46a472bf3ce4fe75705618a7d1e778ad0ca2
yoezequiel <ezequielcasiano15@gmail.com>
2024-05-17 20:15:02 -0300

    add: La Importancia Portafolio en el Mundo de la Programación update: requirements.txt an packets

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template, request
-from flaskext.markdown import Markdown
+import markdown2
 import sqlite3
 
 app = Flask(__name__)
-Markdown(app)
 
-# Función para obtener la conexión a la base de datos
+
 def get_db_connection():
     try:
-        conn = sqlite3.connect('blog.db')
+        conn = sqlite3.connect("blog.db")
         return conn
     except sqlite3.Error as e:
         print(e)
         return None
 
+
 def create_database():
     conn = get_db_connection()
     if conn is not None:
-        conn.execute('''
+        conn.execute(
+            """
             CREATE TABLE IF NOT EXISTS articles (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 title TEXT NOT NULL,
@@ -26,27 +27,34 @@ def create_database():
                 type TEXT NOT NULL,
                 tags TEXT
             )
-        ''')
+        """
+        )
         conn.commit()
         conn.close()
 
-# Llamada a la función create_database() para crear la base de datos si no existe
+
 create_database()
 
-# Rutas
-@app.route('/')
+
+@app.context_processor
+def inject_markdown2():
+    return {"markdown2": markdown2}
+
+
+@app.route("/")
 def index():
     conn = get_db_connection()
     if conn:
         try:
             cursor = conn.cursor()
-
-            cursor.execute("SELECT * FROM articles WHERE type='article' ORDER BY id DESC LIMIT 5")
+            cursor.execute(
+                "SELECT * FROM articles WHERE type='article' ORDER BY id DESC LIMIT 5"
+            )
             latest_articles = cursor.fetchall()
-
-            cursor.execute("SELECT * FROM articles WHERE type='tutorial' ORDER BY id DESC LIMIT 5")
+            cursor.execute(
+                "SELECT * FROM articles WHERE type='tutorial' ORDER BY id DESC LIMIT 5"
+            )
             latest_tutorials = cursor.fetchall()
-
             cursor.close()
             conn.close()
 
@@ -54,9 +62,12 @@ def index():
                 message = "No hay artículos publicados aún"
             else:
                 message = ""
-
-            return render_template('index.html', articles=latest_articles, tutorials=latest_tutorials, message=message)
-
+            return render_template(
+                "index.html",
+                articles=latest_articles,
+                tutorials=latest_tutorials,
+                message=message,
+            )
         except sqlite3.Error as e:
             print(e)
             return "Error en la conexión a la base de datos"
@@ -64,121 +75,115 @@ def index():
         return "Error en la conexión a la base de datos"
 
 
-@app.route('/articles')
+@app.route("/articles")
 def view_all_articles():
     conn = get_db_connection()
     if conn:
         try:
             cursor = conn.cursor()
-
-            cursor.execute("SELECT * FROM articles WHERE type='article' ORDER BY id DESC")
+            cursor.execute(
+                "SELECT * FROM articles WHERE type='article' ORDER BY id DESC"
+            )
             all_articles = cursor.fetchall()
-
             cursor.close()
             conn.close()
-
-            return render_template('all_articles.html', articles=all_articles)
-
+            return render_template("all_articles.html", articles=all_articles)
         except sqlite3.Error as e:
             print(e)
             return "Error en la conexión a la base de datos"
     else:
         return "Error en la conexión a la base de datos"
 
-@app.route('/tutoriales')
+
+@app.route("/tutoriales")
 def view_all_tutorials():
     conn = get_db_connection()
     if conn:
         try:
             cursor = conn.cursor()
-
-            cursor.execute("SELECT * FROM articles WHERE type='tutorial' ORDER BY id DESC")
+            cursor.execute(
+                "SELECT * FROM articles WHERE type='tutorial' ORDER BY id DESC"
+            )
             all_tutorials = cursor.fetchall()
-
             cursor.close()
             conn.close()
-
-            return render_template('all_tutorials.html', tutorials=all_tutorials)
-
+            return render_template("all_tutorials.html", tutorials=all_tutorials)
         except sqlite3.Error as e:
             print(e)
             return "Error en la conexión a la base de datos"
     else:
         return "Error en la conexión a la base de datos"
 
-@app.route('/article/<int:article_id>')
+
+@app.route("/article/<int:article_id>")
 def view_article(article_id):
     conn = get_db_connection()
     if conn:
         try:
             cursor = conn.cursor()
-
             cursor.execute("SELECT * FROM articles WHERE id = ?", (article_id,))
             article = cursor.fetchone()
-
             cursor.close()
             conn.close()
-
-            return render_template('article.html', article=article)
-
+            return render_template("article.html", article=article)
         except sqlite3.Error as e:
             print(e)
             return "Error en la conexión a la base de datos"
     else:
         return "Error en la conexión a la base de datos"
 
-@app.route('/search')
+
+@app.route("/search")
 def search():
-    keyword = request.args.get('keyword', '')
+    keyword = request.args.get("keyword", "")
 
     conn = get_db_connection()
     if conn:
         try:
             cursor = conn.cursor()
-
-            cursor.execute("SELECT * FROM articles WHERE title LIKE ?", ('%' + keyword + '%',))
+            cursor.execute(
+                "SELECT * FROM articles WHERE title LIKE ?", ("%" + keyword + "%",)
+            )
             articles = cursor.fetchall()
-
             cursor.close()
             conn.close()
-
-            return render_template('search.html', articles=articles)
-
+            return render_template("search.html", articles=articles)
         except sqlite3.Error as e:
             print(e)
             return "Error en la conexión a la base de datos"
     else:
         return "Error en la conexión a la base de datos"
 
-@app.route('/create_article_yoezequiel_011358', methods=['GET', 'POST'])
+
+@app.route("/create_article_yoezequiel_011358", methods=["GET", "POST"])
 def create_article():
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
-            title = request.form['title']
-            content = request.form['content']
-            article_type = request.form['type']  # New field for article type
-            tags = request.form['tags']  # New field for article tags
-            image = request.files.get('image')  # Usamos get() en lugar de ['image'] para obtener el archivo opcionalmente
-
+            title = request.form["title"]
+            content = request.form["content"]
+            article_type = request.form["type"]
+            tags = request.form["tags"]
+            image = request.files.get("image")
             if image:
-                # Guardar la imagen en tu sistema de archivos
-                image.save('static/images/' + image.filename)
-
+                image.save("static/images/" + image.filename)
             conn = get_db_connection()
             if conn:
                 try:
                     cursor = conn.cursor()
-
-                    # Insertar el artículo en la base de datos
-                    cursor.execute("INSERT INTO articles (title, content, image, type, tags) VALUES (?, ?, ?, ?, ?)",
-                                (title, content, image.filename if image else None, article_type, tags))
+                    cursor.execute(
+                        "INSERT INTO articles (title, content, image, type, tags) VALUES (?, ?, ?, ?, ?)",
+                        (
+                            title,
+                            content,
+                            image.filename if image else None,
+                            article_type,
+                            tags,
+                        ),
+                    )
                     conn.commit()
-
                     cursor.close()
                     conn.close()
-
                     return "Artículo creado exitosamente"
-
                 except sqlite3.Error as e:
                     print(e)
                     return "Error en la conexión a la base de datos"
@@ -189,11 +194,13 @@ def create_article():
             print(e)
             return "Error en la conexión a la base de datos"
 
-    return render_template('create_article.html')
+    return render_template("create_article.html")
+
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
